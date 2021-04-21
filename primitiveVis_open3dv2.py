1f8f9612931db0ccea992f86ff6082cad408bae0
Alejandro Mossi <alejandro.mossi@gmail.com>
2021-04-21 12:29:17 +0200

    lars_changes

--- primitiveVis_open3dv2.py
+++ primitiveVis_open3dv2.py
@@ -1,9 +1,3 @@
-import sys
-from PyQt5.QtWidgets import (QPushButton, QDialog, QTreeWidget,
-                             QTreeWidgetItem, QVBoxLayout,
-                             QHBoxLayout, QFrame, QLabel,
-                             QApplication,QListWidget,QScrollBar)
-
 from PyQt5.QtWidgets import * 
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore, QtGui 
@@ -13,13 +7,13 @@ import sys
 import open3d as o3d
 import pandas as pd
 import numpy as np
+#import laspy as lp
 import random
-import time
 
 
-class Window: 
+class Window(QMainWindow): 
 
-    def __init__(self,dataframe,columns,width=2000,height=2000,color_dic=None): 
+    def __init__(self,dataframe,color_dic=None): 
         
         super().__init__() 
         
@@ -31,204 +25,103 @@ class Window:
         
         """
         # setting title 
-
+        self.setWindowTitle("Primitive Visualizer Open3D ") 
+        
         self.dataframe = dataframe
         self.color_dic = color_dic
-        self.dic_pointclouds ={c:self.pass_data(c) for c in columns}
-        print('Data Loaded')
-
-        self.vis = Visualizer(self.dic_pointclouds,columns,width=2000,height=2000,color_dic=None)
-        self.collapse = CollapsibleDialog(self.dic_pointclouds,vis=self.vis)
-        self.widget_lists = self.collapse.widget_lists
-        self.collapse.show()
         
-        for l in self.widget_lists:
-            l.list_widget.itemSelectionChanged.connect(l.selectionChanged)
+        self.dic_pointclouds = self.pass_data()
 
-        self.vis.execute()
+        # setting geometry 
+        self.setGeometry(100, 100, 200, 800) 
+          
+        # calling method 
+        self.list_widget = self.UiComponents()
+        self.list_widget.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.add_items()
+        # showing all the widgets 
+        self.opened = False
+        self.list_widget.itemClicked.connect(self.visualizeItems)
 
-    def pass_data(self,column):
+
+        #self.show()
+        
+  
+  
+    # method for components 
+    def UiComponents(self): 
+  
+        # creating a QListWidget 
+        list_widget = QListWidget(self) 
+  
+        # setting geometry to it 
+        list_widget.setGeometry(10, 10, 175, 775) 
+        
+        # scroll bar 
+        scroll_bar = QScrollBar(self) 
+  
+        # setting style sheet to the scroll bar 
+        scroll_bar.setStyleSheet("background : black;") 
+  
+        # adding extra scroll bar to it 
+        list_widget.addScrollBarWidget(scroll_bar, Qt.AlignLeft)
+        return list_widget
+  
+        # list widget items 
+    
+    def pass_data(self):
         r = lambda: random.randint(0,255)
-        gene_grp = self.dataframe.groupby(column)
+
+        gene_grp = self.dataframe.groupby('gene')
         gene_grp = {g[0]:g[1] for g in gene_grp}
+
         dic_coords = {}
         for gene in gene_grp:
             coords = gene_grp[gene].loc[:,['c_px_microscope_stitched','r_px_microscope_stitched']].to_numpy()
             coords = np.vstack([coords[:,0],coords[:,1],np.ones(coords.shape[0])]).T
             if self.color_dic == None:
-                col = np.array([(r(),r(),r())]*coords.shape[0])/255 
+                col = np.array([(r(),r(),r())]*coords.shape[0])/255
+                
             else:
                 col = np.array([color_dic[gene]]*coords.shape[0])/255
-            dic_coords[str(gene)] = (coords,col)
+                
+            pcd = o3d.geometry.PointCloud()
+            pcd.points = o3d.utility.Vector3dVector(coords)
+            pcd.colors = o3d.utility.Vector3dVector(col)    
+            
+            dic_coords[gene] = pcd
+
         return dic_coords
     
-class Visualizer:
-    def __init__(self,dic_pointclouds,columns,width=2000,height=2000,color_dic=None):
-        
-        self.visM = o3d.visualization.Visualizer()
-        self.visM.create_window(height=height,width=width,top=0,left=500)
-        
-        self.dic_pointclouds= dic_pointclouds
-        
-        self.allgenes = np.concatenate([self.dic_pointclouds[columns[0]][i][0] for i in self.dic_pointclouds[columns[0]].keys()])
-        self.allcolors = np.concatenate([self.dic_pointclouds[columns[0]][i][1] for i in self.dic_pointclouds[columns[0]].keys()])
-        self.pcd = o3d.geometry.PointCloud()
-        self.pcd.points = o3d.utility.Vector3dVector(self.allgenes)
-        self.pcd.colors = o3d.utility.Vector3dVector(self.allcolors)   
-        self.visM.add_geometry(self.pcd)
-        opt = self.visM.get_render_option()
-        opt.background_color = np.asarray([0, 0, 0])
+    #def visualize_data(self):
     
-    def execute(self):
-        self.visM.run()
-        self.visM.destroy_window()
-        
-        
-
-class SectionExpandButton(QPushButton):
-    """a QPushbutton that can expand or collapse its section
-    """
-    def __init__(self, item, text = "", parent = None):
-        super().__init__(text, parent)
-        self.section = item
-        self.clicked.connect(self.on_clicked)
-
-    def on_clicked(self):
-        """toggle expand/collapse of section by clicking
-        """
-        if self.section.isExpanded():
-            self.section.setExpanded(False)
-        else:
-            self.section.setExpanded(True)    
-            
-class ListWidget(QWidget):
-    def __init__(self,subdic,section,vis):
-        super().__init__()
-        
-        # creating a QListWidget 
-        self.list_widget = QListWidget()
-        
-        # scroll bar 
-        self.subdic = subdic
-        self.section = section
-        self.selected = False
-        self.vis = vis
-        scroll_bar = QScrollBar() 
-        # setting style sheet to the scroll bar 
-        scroll_bar.setStyleSheet("background : black;") 
-        # adding extra scroll bar to it 
-        self.list_widget.addScrollBarWidget(scroll_bar, Qt.AlignLeft)
-        self.add_items()
-
-        self.list_widget.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        
-        #self.list_widget.itemClicked(self.on_clicked)
-        
     def add_items(self):
-        for e in self.subdic:
-            i = QListWidgetItem(str(e)) 
-            c = self.subdic[e][1][0,:]*255
-            i.setBackground(QColor(c[0],c[1],c[2],120))
-            self.list_widget.addItem(i)
-        # adding items to the list widget '''
+        items = [QListWidgetItem(gene) for gene in self.dic_pointclouds]
+        for it in items:
+            self.list_widget.addItem(it)
+        # adding items to the list widget 
         
-    def selectionChanged(self):
-        self.selected = [i.text() for i in self.list_widget.selectedItems()]
-
+    def visualizeItems(self):
+        items = self.list_widget.selectedItems()
         genes = []
-        points, colors = [], [ ]
-        for i in self.selected:
-            ps,cs = self.vis.dic_pointclouds[self.section][i]
-            points.append(ps)
-            colors.append(cs)
-            
-        self.vis.pcd.points = o3d.utility.Vector3dVector(np.concatenate(points))
-        self.vis.pcd.colors = o3d.utility.Vector3dVector(np.concatenate(colors))
-        self.vis.visM.update_geometry(self.vis.pcd)
-        self.vis.visM.poll_events()
-        self.vis.visM.update_renderer()
-    
+        point_clouds = []
+        for i in range(len(items)):
+            genes.append(str(self.list_widget.selectedItems()[i].text()))
+            point_clouds.append(self.dic_pointclouds[str(self.list_widget.selectedItems()[i].text())])
 
-class CollapsibleDialog(QDialog):
-    """a dialog to which collapsible sections can be added;
-    subclass and reimplement define_section() to define sections and
-        add them as (title, widget) tuples to self.sections
-    """
-    def __init__(self,dic,vis):
-        super().__init__()
-        self.tree = QTreeWidget()
-        self.tree.setHeaderHidden(True)
-        self.vis = vis
-        layout = QVBoxLayout()
-        layout.addWidget(self.tree)
-        self.setLayout(layout)
-        self.setGeometry(100, 100, 200, 800) 
-        self.tree.setIndentation(0)
-        self.dic = dic
+        self.vis = o3d.visualization.Visualizer()
+        #if self.opened == False:
+        self.vis.create_window('Genes')
         
-        self.widget_lists = []
-        self.sections = {}
-        for x in self.dic:
-            self.define_section(x)
-            
-        self.add_sections()
-          
-    def add_sections(self):
-        """adds a collapsible sections for every 
-        (title, widget) tuple in self.sections
-        """
-        for title in self.sections:
-            widget = self.sections[title]
-            button1 = self.add_button(title)
-            section1 = self.add_widget(button1, widget)
-            button1.addChild(section1)       
+        self.opened=True
+        for x in point_clouds:
+            self.vis.add_geometry(x)
 
-    def define_section(self,title):
-        """reimplement this to define all your sections
-        and add them as (title, widget) tuples to self.sections
-        """
-        widget = QFrame(self.tree)
-        layout = QHBoxLayout(widget)
-
-        #layout.addWidget(QLabel("Bla"))
-        lw = ListWidget(self.dic[title],title,self.vis)
-        list_widget = lw.list_widget
-        layout.addWidget(list_widget)
-        self.sections[title]= widget
-        self.widget_lists.append(lw)
+        opt = self.vis.get_render_option()
+        opt.background_color = np.asarray([0, 0, 0])
+        self.vis.run()
         
-    def add_button(self, title):
-        """creates a QTreeWidgetItem containing a button 
-        to expand or collapse its section
-        """
-        item = QTreeWidgetItem()
-        self.tree.addTopLevelItem(item)
-        self.tree.setItemWidget(item, 0, SectionExpandButton(item, text = title))
-        return item
-
-    def add_widget(self, button, widget):
-        """creates a QWidgetItem containing the widget,
-        as child of the button-QWidgetItem
-        """
-        section = QTreeWidgetItem(button)
-        section.setDisabled(True)
-        self.tree.setItemWidget(section, 0, widget)
-        return section
-        
-
-def visualize(dataframe,columns,width=2000,height=2000,color_dic=None):
-    QtWidgets.QApplication.setStyle('Fusion')
-    App = QtWidgets.QApplication.instance()
-    if App is None:
-        App = QtWidgets.QApplication(sys.argv)
-    else:
-        print('QApplication instance already exists: %s' % str(App))
-
-    window = Window(dataframe,columns,width,height,color_dic) 
-    sys.exit(App.exec_())
-
-        
-
+        self.vis.destroy_window()
         
         
 
