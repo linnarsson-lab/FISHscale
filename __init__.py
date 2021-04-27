fc038f3b636c49f1a85bd968c0d73ee8bbf2f2ae
Alejandro Mossi <alejandro.mossi@gmail.com>
2021-04-27 16:14:46 +0200

    minor

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
-try:
-    from .visualization import *
-except:
-    print('Import Error: visualization')
+from .visualization import *
 from .utils import *
 from .clustering import *
 from .graphNN import *
