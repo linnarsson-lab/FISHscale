d5f54f2f021ca8358e7fc843d708effeb9471b61
alejandro <alejandro@monod.mbb.ki.se>
2021-06-14 15:47:56 +0200

    minor

--- graphNN/models.py
+++ graphNN/models.py
@@ -128,7 +128,7 @@ class SAGE(pl.LightningModule):
         return n_loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
         return optimizer
 
     def training_step(self, batch, batch_idx):
