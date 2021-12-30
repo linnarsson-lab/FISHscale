9582f6efd4ab68b113faf409af60b55de66d1248
Alejandro Mossi <alejandro.mossi@gmail.com>
2021-12-30 21:06:51 +0100

    minor

--- FISHscale/graphNN/models.py
+++ FISHscale/graphNN/models.py
@@ -95,23 +95,23 @@ class SAGELightning(LightningModule):
                 fake_nghs[l] = merged_genes
 
             fake_nghs = [fake_nghs[int(l)] for l in predictions]
-            fake_nghs = th.stack(fake_nghs) + local_nghs
+            fake_nghs = th.stack(fake_nghs) #+ local_nghs
             #print(th.log(fake_nghs/fake_nghs.sum(axis=0)))
             #kl_prob = self.kl(th.log(fake_nghs/fake_nghs.sum(axis=0)),self.reference.T).sum()
             bone_fight_loss = -F.cosine_similarity(probabilities_unlab @ self.reference.T.to(self.device), fake_nghs,dim=1).mean()
             bone_fight_loss += -F.cosine_similarity(probabilities_unlab @ self.reference.T.to(self.device), fake_nghs,dim=0).mean()
             
             q = th.ones(probabilities_unlab.shape[1],device=self.device)/probabilities_unlab.shape[1]
-            p = th.log(probabilities_unlab.sum(axis=0)/probabilities_unlab.shape[0])
+            p = th.log(probabilities_unlab.sum(axis=0))
             #assigned_molecules = th.tensor([assigned_molecules[x]for x in range(probabilities_unlab.shape[1])],dtype=th.float32) +1
             #p = p/assigned_molecules
-            p = th.log(p)
+            p = th.log(p/p.sum())
             # Adjust probabilities for the number of molecules assigned to each cell type:
             #kl_loss_uniform = self.kl(p,self.p.to(self.device))
             kl_loss_uniform = self.kl(p,q).sum()
 
             #loss2 = bone_fight_loss + kl_divergence_z
-            loss += bone_fight_loss.mean() #+ kl_divergence_z.mean()
+            loss += bone_fight_loss + kl_loss_uniform
 
         #self.log('bone_fight_loss', bone_fight_loss, prog_bar=True, on_step=True, on_epoch=True)
         self.log('KLuniform', kl_loss_uniform, prog_bar=True, on_step=True, on_epoch=True)
