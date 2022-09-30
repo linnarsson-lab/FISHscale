8b666dd862cb985cdd976397dd429a3096dbbd30
alejandro <alejandro@monod.mbb.ki.se>
2022-09-30 18:57:11 +0200

    fixing PCI and send to shojio

--- FISHscale/graphNN/graph_pci.py
+++ FISHscale/graphNN/graph_pci.py
@@ -25,9 +25,8 @@ class GraphPCI:
     def load_segmentation(self, segmentation_path:str, output_name:str) -> None:
         #os.path.join(self.folder,'/Segmentation/*.parquet')
         #os.path.join(self.folder,"AMEXP20211210_EEL_SL001A_S2_RNA_transformed_assigned_GATsegment.parquet")
-
+        print(segmentation_path)
         df = dd.read_parquet(segmentation_path).compute()
-        df.to_parquet(output_name)
         df= df.rename(columns={"g":'Gene', 'Segmentation':'label'})
         print(df.head())
         labels = df.label.values
@@ -39,6 +38,7 @@ class GraphPCI:
 
         filter_controls = [True if g.count('Control') ==0 else False for g in df.Gene]
         self.graph_df = df[filter_controls]
+        df.to_parquet(output_name)
 
     def run(self, folder, analysis_name):
         pci = pciSeq.fit(self.graph_df, self.scRNAseq)
