c99518bc1af70d2f77a310aecf5533d971ef1ffa
Alejandro Mossi <alejandro.mossi@gmail.com>
2022-11-04 12:56:56 +0100

    fixed?

--- FISHscale/utils/dataset.py
+++ FISHscale/utils/dataset.py
@@ -385,7 +385,7 @@ class Dataset(Regionalize, Iteration, ManyColors, GeneCorr, GeneScatter, Attribu
 
                 #result_grp = Parallel(
                 #    n_jobs=multiprocessing.cpu_count(), backend='threading',max_nbytes=None)(delayed(_cell_extract)([part, self.unique_genes]) for _, part in partition_filt.groupby('Segmentation'))
-                with ThreadPoolExecutor(max_workers=12) as executor:
+                with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
                     task = [(part, self.unique_genes) for _, part in partition_filt.groupby('Segmentation')]
                     results_grp = executor.map(_cell_extract, task)
 
@@ -511,7 +511,7 @@ def _segmentation_dots(partition, func):
     new_results_resegmentation = []
     count = 0
 
-    with ThreadPoolExecutor(max_workers=12) as executor:
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
         logging.info('Starting resegmentation, gouping by tmp_segment...')
         task = [part for _, part in partition.groupby('tmp_segment')]
         logging.info('Starting resegmentation, gouping done.')
